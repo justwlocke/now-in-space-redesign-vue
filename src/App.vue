2cc16bee636f4bf50b6befd984596357e8da6d0c
Justin Locke <84145548+justwlocke@users.noreply.github.com>
2022-08-01 16:40:19 -0500

    Attempted adding the astronaut card But failed in the responsiveness sector. The height of the div is still changing even though it shouldn't be, because the page container height isn't.

--- src/App.vue
+++ src/App.vue
@@ -54,52 +54,44 @@
         </div>
       </div>
 
-      <!--Astronaut Card-->
-      <div class="Astronaut" id="Cards">
-        <div class="astronaut-container">
-          <img
-            class="d-inline"
-            src="./Images/Astronauts/DefaultAstronautCard2.png"
-            alt="First slide"
-          />
-          <div class="AstroImage CardCrop"></div>
-          <div class="AstronautName">Samantha Cristoforetti</div>
-        </div>
-      </div>
-
       <!--Astronaut Carousel -->
 
-      <swiper
-        :slidesPerView="4"
-        :spaceBetween="30"
-        :pagination="{
-          clickable: true,
-        }"
-        :modules="modules"
-        class="mySwiper"
-      >
-        <swiper-slide>
-          <div class="Astronaut" id="Cards">
-            <div class="astronaut-container">
-              <img
-                class="d-inline"
-                src="./Images/Astronauts/DefaultAstronautCard2.png"
-                alt="First slide"
-              />
-              <div class="AstroImage CardCrop"></div>
-              <div class="AstronautName">Samantha Cristoforetti</div>
+      <div id="spacer"></div>
+
+      <div class="carousel">
+        <swiper
+          :slidesPerView="4"
+          :spaceBetween="30"
+          :pagination="{
+            clickable: true,
+          }"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide>
+            <div class="Astronaut" id="Cards">
+              <div class="astronaut-container">
+                <img
+                  class="d-inline"
+                  src="./Images/Astronauts/DefaultAstronautCard2.png"
+                  alt="First slide"
+                />
+                <div class="AstroImage CardCrop"></div>
+                <div class="AstronautName">Samantha Cristoforetti</div>
+              </div>
             </div>
-          </div>
-        </swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-        <swiper-slide>Slide 4</swiper-slide>
-        <swiper-slide>Slide 5</swiper-slide>
-        <swiper-slide>Slide 6</swiper-slide>
-        <swiper-slide>Slide 7</swiper-slide>
-        <swiper-slide>Slide 8</swiper-slide>
-        <swiper-slide>Slide 9</swiper-slide>
-      </swiper>
+          </swiper-slide>
+          <swiper-slide>Slide 2</swiper-slide>
+          <swiper-slide>Slide 3</swiper-slide>
+          <swiper-slide>Slide 4</swiper-slide>
+          <swiper-slide>Slide 5</swiper-slide>
+          <swiper-slide>Slide 6</swiper-slide>
+          <swiper-slide>Slide 7</swiper-slide>
+          <swiper-slide>Slide 8</swiper-slide>
+          <swiper-slide>Slide 9</swiper-slide>
+        </swiper>
+      </div>
+      <!--End of Carousel-->
     </div>
   </div>
 </template>
