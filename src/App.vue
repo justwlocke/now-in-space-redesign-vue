0b0d663348741fa30f326b8599e6963ee6ad4c10
Justin Locke <84145548+justwlocke@users.noreply.github.com>
2022-08-12 15:47:05 -0500

    Carousel Shenanagins Succesfully figured out the syntax to declare our own nav buttons with Vue Swiper. Also tested out different aspect ratios for the astronaut cards.

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,28 @@
       <div class="expeditionMonitors">
         <div class="monitor" id="MonitorLeft">
           <img src="./Images/NowInSpaceMonitorLeft.png" />
+          <div class="patch" id="PatchLeft">
+            <img
+              src="https://www.nasa.gov/specials/kidsclub/nowinspace/expedition67-crew4/img/slide_12.png"
+            />
+          </div>
         </div>
         <div class="monitor" id="MonitorMiddle">
-          <!--<img src="./Images/NowInSpaceMonitorMiddle.png" />-->
+          <img src="./Images/NowInSpaceMonitorMiddle.png" />
+          <div class="patch" id="PatchMiddle">
+            <img
+              src="https://www.nasa.gov/specials/kidsclub/nowinspace/expedition67-crew4/img/slide_12.png"
+            />
+          </div>
         </div>
-        <div class="monitor" id="MonitorRight"></div>
+        <div class="monitor" id="MonitorRight">
+          <img src="./Images/NowInSpaceMonitorRight.png" />
+          <div class="patch" id="PatchRight">
+            <img
+              src="https://www.nasa.gov/specials/kidsclub/nowinspace/expedition67-crew4/img/slide_12.png"
+            />
+          </div>
+        </div>
       </div>
 
       <!--Astronaut Carousel -->
@@ -23,6 +40,10 @@
         <swiper
           :slidesPerView="4"
           :spaceBetween="30"
+          :navigation="{
+            nextEl: '.swiper-button-next-u',
+            prevEl: '.swiper-button-prev-u',
+          }"
           :pagination="{
             clickable: true,
           }"
@@ -34,7 +55,7 @@
               <div class="astronaut-container">
                 <img
                   class="d-inline"
-                  src="./Images/Astronauts/DefaultAstronautCard2.png"
+                  src="./Images/Astronauts/CardBackgrounds/DefaultAstronautCard2.png"
                   alt="First slide"
                 />
                 <div class="AstroImage CardCrop"></div>
@@ -42,36 +63,73 @@
               </div>
             </div>
           </swiper-slide>
-          <swiper-slide>Slide 2 But then some extra text happens</swiper-slide>
-          <swiper-slide>Slide 3</swiper-slide>
-          <swiper-slide>Slide 4</swiper-slide>
+          <swiper-slide>
+            <div class="Cards">
+              <div class="astronaut-container">
+                <img
+                  class="d-inline"
+                  src="./Images/Astronauts/CardBackgrounds/CardBackground600x750.png"
+                  alt="third slide"
+                />
+                <div class="AstroImage CardCrop2"></div>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="Cards">
+              <div class="astronaut-container">
+                <img
+                  class="d-inline"
+                  src="./Images/Astronauts/CardBackgrounds/CardBackground585x780G.png"
+                  alt="third slide"
+                />
+                <div class="AstroImage CardCrop3"></div>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="Cards">
+              <div class="astronaut-container">
+                <img
+                  class="d-inline"
+                  src="./Images/Astronauts/CardBackgrounds/CardBackground580x870.png"
+                  alt="Fourth slide"
+                />
+                <div class="AstroImage CardCrop4"></div>
+              </div>
+            </div>
+          </swiper-slide>
           <swiper-slide>Slide 5</swiper-slide>
           <swiper-slide>Slide 6</swiper-slide>
           <swiper-slide>Slide 7</swiper-slide>
           <swiper-slide>Slide 8</swiper-slide>
           <swiper-slide>Slide 9</swiper-slide>
         </swiper>
-      </div>
 
-      <!--End of Carousel-->
+        <div class="swiper-button-next swiper-button-next-u"></div>
+        <div class="swiper-button-prev swiper-button-prev-u"></div>
+
+        <!--End of Carousel-->
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 // Import Swiper Vue.js components
+// import required modules
+import { Navigation, Pagination } from "swiper";
+
 import { Swiper, SwiperSlide } from "swiper/vue";
 
 // Import Swiper styles
 import "swiper/css";
-
+import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import "./CSS/style.css";
 
 import "./CSS/page.css";
-
-// import required modules
-import { Pagination } from "swiper";
 
 export default {
   components: {
@@ -80,8 +138,9 @@ export default {
   },
   setup() {
     return {
-      modules: [Pagination],
+      modules: [Navigation, Pagination],
     };
   },
 };
 </script>
+
